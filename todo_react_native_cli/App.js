bc8cf988c5beac8900ffd909af8311b264eb9c0c
lisa710junyi <83738368+lisa710junyi@users.noreply.github.com>
2022-02-12 14:40:00 -0500

    README

--- todo_react_native_cli/App.js
+++ todo_react_native_cli/App.js
@@ -21,37 +21,57 @@ import {
 } from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Style Test');
-
-  const onClickHandler = () => {
-    setName('Style Test is Done');
-  };
-
   return (
     <View style={styles.body}>
-      <Text style={styles.text}> {name}</Text>
-      <Button title="Update State" onPress={onClickHandler}></Button>
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
-    width: '100%',
-    height: '50%',
+    flex: 1,
     backgroundColor: '#FAEBD7',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
     borderWidth: 10,
     borderColor: '#DC143C',
     borderRadius: 10,
+    flexDirection: 'row',
   },
 
+  view1: {
+    flex: 2,
+    backgroundColor: '#6495ED',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 1,
+    backgroundColor: '#FF00FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 1,
+    backgroundColor: '#00BFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   text: {
     color: '#000000',
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
+    textTransform: 'uppercase',
   },
 });
 
